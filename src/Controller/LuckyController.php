ddff38f11694f946cd519318df1f0298287db707
yaroslav <yanayug@pushwoosh.com>
2020-04-01 10:50:46 +0700

    Add first controller

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController
+{
+    /**
+     * @return Response
+     * @throws \Exception
+     */
+    public function number()
+    {
+        $number = random_int(0, 100);
+        return new Response('<html><body>Lucky number: '.$number.'</body></html>');
+    }
+}
