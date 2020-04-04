383bbb855a7f46851655bbc71034fc138f862f0e
yaroslav <yanayug@pushwoosh.com>
2020-04-04 12:01:47 +0700

    Add index template

---
+++ src/Controller/MainController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class MainController extends AbstractController
+{
+    /**
+     * @param $word
+     * @return Response
+     */
+    public function index()
+    {
+        return $this->render('index.html.twig');
+    }
+}
