1dadee2c596f0ffd2253f129c1b702854a2db256
yaroslavnayug <yaroslav.nayug@gmail.com>
2020-04-14 20:24:42 +0700

    Add create book method

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -6,13 +6,22 @@ namespace App\Controller;
 
 use App\Entity\Book;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class BookController extends AbstractController
 {
-    public function add()
+    public function add(Request $request)
     {
+        $book = new Book();
+        $book->setTitle($request->get('title'));
+        $book->setAuthor($request->get('author'));
 
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($book);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('books');
     }
 
     /**
